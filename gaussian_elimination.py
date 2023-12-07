1ff7fa3891375d95f646d8182cfbd63c8e2a5a75
kaouther <kaoutherboutheldja@gmail.com>
2023-12-07 17:16:58 +0100

    Fix issues

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -3,13 +3,13 @@ from back_substitution import back_substitution
 from triangularization import triangularization
 
 def  gaussian_elimination(matrix_A, vector_B):
-    # Combine matrix A and vector B into an augmented matrix
-    augmented_matrix = np.column_stack((matrix_A, vector_B))
+    # Combine matrix A and vector B into an matrix
+    matrix = np.column_stack((matrix_A, vector_B))
 
     # Call the Gaussian elimination function
-    modified_matrix, modified_vector = triangularization(augmented_matrix[:, :-1], augmented_matrix[:, -1])
+    modified_matrix, modified_vector = triangularization(matrix[:, :-1], matrix[:, -1])
 
     # Perform back-substitution
-    solution_vector = back_substitution(modified_matrix, modified_vector)
+    arr_sol = back_substitution(modified_matrix, modified_vector)
 
-    return solution_vector
+    return arr_sol
