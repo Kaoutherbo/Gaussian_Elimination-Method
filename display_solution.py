65c03d301ce627d14d6b6e85aa335876c831f960
kaouther <kaoutherboutheldja@gmail.com>
2023-12-08 16:33:13 +0100

    Add some changes

--- display_solution.py
+++ display_solution.py
@@ -3,4 +3,4 @@ def display_solution(arr_sol, matrix_size):
     print("\nThe solution of this system is:")
     for i in range(matrix_size):
         print(f"X{i+1} = {arr_sol[i]:.2f}", end="\t")
-
+        
